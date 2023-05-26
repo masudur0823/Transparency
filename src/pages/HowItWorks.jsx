e43fbf4fad6a95b1f59b55827098eb91cdeb5264
masudur0823 <masudur0823@gmail.com>
2023-05-26 12:17:18 +0600

    e

--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -1,5 +1,15 @@
+import img1 from "../assets/images/1.jpg"
+import SectionHeader from "../components/SectionHeader"
+import SectionMainLayout from "../components/SectionMainLayout"
 export default function HowItWorks() {
     return (
-      <div>HowItWorks</div>
+      <div>
+        <SectionMainLayout>
+          <SectionHeader title="How it works" />
+          <div className="container mx-auto mt-10">
+            <img src={img1} alt="" className="w-full" />
+          </div>
+        </SectionMainLayout>
+      </div>
     )
   }
